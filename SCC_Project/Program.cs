b2a217af8c64d84a2e2d47d9497974d2562e33c6
TayYH <tayyh@utar.edu.my>
2018-07-16 16:18:57 +0800

    This is the first commit

---
+++ SCC_Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCC_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("This is the program for SCC");
+            Console.WriteLine("Hello Everyone!!!!");
+        }
+    }
+}
